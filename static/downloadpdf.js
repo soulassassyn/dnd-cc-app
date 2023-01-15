0bea3870cf9cb30d8474c648fc13028e15c33c8c
Kyle Hodge <soulassassyn@gmail.com>
2023-01-15 14:25:21 -0700

    jsPDF and depencies added

--- static/downloadpdf.js
+++ static/downloadpdf.js
@@ -1,11 +1,16 @@
+// import jsPDF from "jspdf";
+
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("download-pdf-button").addEventListener("click", function(){
+    const downloadButton = getElementById("download-pdf-button");
+    console.log(downloadButton);
+    downloadButton.addEventListener("click", function(){
+        console.log("Click");
         var pdfPage = document.querySelector('.PDF-Page');
         var pdf = new jsPDF();
         pdf.setPageSize('letter');
         pdf.fromHTML(pdfPage.innerHTML, 15, 15, {
         width: 170
-        });
+    });
         pdf.save('my-pdf.pdf');
     });
 });
