9b76ff28c1e26ea3661cb71edd7775f4a15b30de
Kyle Hodge <soulassassyn@gmail.com>
2023-01-15 14:59:11 -0700

    Removed jsPDF and all depencies

--- static/app.js
+++ static/app.js
@@ -1,7 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-    document.getElementById("pdf-button").addEventListener("click", function() {
-        window.location = '/pdf';
-    });
     
 });
