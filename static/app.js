c6b4a5659f7228100f59989653d52b4d33d7876a
Kyle Hodge <soulassassyn@gmail.com>
2023-01-16 15:03:53 -0700

    Footer resizes appropriately with window height

--- static/app.js
+++ static/app.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const element = document.getElementById('pdf');
-    html2pdf(element);
+    function checkHeight() {
+        let totalHeight = 0;
+        if (document.body.scrollHeight > window.innerHeight) {
+            totalHeight = document.body.scrollHeight;
+        } else {
+            totalHeight = window.innerHeight;
+        }
+        document.documentElement.style.setProperty('--total-height', totalHeight + 'px');
+    }
+    
+    window.onload = checkHeight;
+    window.addEventListener("resize", checkHeight);
 });
